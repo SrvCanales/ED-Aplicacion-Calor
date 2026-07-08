03bba7dd607fd49d5664c240aa7f44058cb00fbb
SrvCanales <srvcanales@gmail.com>
2026-07-08 06:24:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -349,15 +349,15 @@ local_dict={'pi': sp.pi}
       u_final_num = sp.simplify(w + v_sol_num)
       u_num_func = sp.lambdify((x, t), u_final_num, modules=['numpy', 'math'])
 
-    st.session_state.math_data = {
+       st.session_state.math_data = {
        L': L, 'alpha': alpha, 'F': F, 'A': A, 'B': B, 'f': f,
        w': w, 'F_tilde': F_tilde, 'f_tilde': f_tilde,
        'lam_n': lam_n, 'phi_n': phi_n, 'q_n_expr': q_n_expr,
        'u_num_func': u_num_func, 'L_num': float(L.evalf()) }
-    return True
-except Exception as e:
-    st.error(f"Error en los cálculos matemáticos: {e}")
-    return False
+       return True
+   except Exception as e:
+      st.error(f"Error en los cálculos matemáticos: {e}")
+      return False
 
 
 # =============================================================================
